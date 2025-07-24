1297dcf256fdcb048ef01e24a5ca71d625851d84
joeribreedveld <joeri.breedveld123@gmail.com>
2025-07-24 21:02:51 +0200

    feat: window and drawing paddles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+const PADDLE_WIDTH: f32 = 24.0;
+const PADDLE_HEIGHT: f32 = 128.0;
+const PADDLE_COLOR: Color = WHITE;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Pong".to_string(),
+        window_width: 800,
+        window_height: 600,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+
+        draw_rectangle(
+            32.0,
+            screen_height() / 2.0,
+            PADDLE_WIDTH,
+            PADDLE_HEIGHT,
+            WHITE,
+        );
+
+        draw_rectangle(
+            screen_width() - 32.0,
+            screen_height() / 2.0,
+            PADDLE_WIDTH,
+            PADDLE_HEIGHT,
+            PADDLE_COLOR,
+        );
+
+        next_frame().await;
+    }
 }
